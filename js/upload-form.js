46999091772f400bb1eb106fcb4100ad530c5d0a
Natalie Beloeva <nbeloeva@gmail.com>
2021-07-12 20:19:38 +0300

    Adds file reader for image upload

--- js/upload-form.js
+++ js/upload-form.js
@@ -5,6 +5,7 @@ import {resetEffects} from './upload-effects.js';
 import {createRequest} from './api.js';
 import {renderPopup} from './upload-form-popup.js';
 
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 const UPLOAD_URL = 'https://23.javascript.pages.academy/kekstagram';
 
 const uploadForm = document.querySelector('.img-upload__form');
@@ -12,6 +13,8 @@ const uploadFile = uploadForm.querySelector('#upload-file');
 const uploadOverlay = uploadForm.querySelector('.img-upload__overlay');
 const uploadCancel = uploadForm.querySelector('.img-upload__cancel');
 const uploadHashtag = uploadForm.querySelector('.text__hashtags');
+const uploadFormImg = uploadForm.querySelector('.img-upload__preview img');
+const uploadFormEffectPreviews = uploadForm.querySelectorAll('.effects__preview');
 
 const resetUploadEdit = () => {
   resetScale();
@@ -19,12 +22,12 @@ const resetUploadEdit = () => {
 };
 
 const closeUploadForm = () => {
-  uploadOverlay.classList.add('hidden');
-  document.body.classList.remove('modal-open');
-
   setInputValid();
   resetUploadEdit();
   uploadForm.reset();
+
+  uploadOverlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
 
   uploadHashtag.removeEventListener('input', hashtagInputHandler);
   uploadCancel.removeEventListener('click', closeUploadForm);
@@ -39,10 +42,10 @@ const documentKeydownHandler = (evt) => {
 };
 
 const openUploadForm = () => {
+  resetUploadEdit();
+
   uploadOverlay.classList.remove('hidden');
   document.body.classList.add('modal-open');
-
-  resetUploadEdit();
 
   uploadHashtag.addEventListener('input', hashtagInputHandler);
   document.addEventListener('keydown', documentKeydownHandler);
@@ -53,8 +56,29 @@ const openUploadForm = () => {
   });
 };
 
+const uploadImage = () => {
+  const file = uploadFile.files[0];
+  const fileName = file.name.toLowerCase();
+  const matches = FILE_TYPES.some((item) => fileName.endsWith(item));
+
+  uploadFormImg.src = '';
+  uploadFormEffectPreviews.forEach((item) => item.style.backgroundImage = 'none');
+
+  if (matches) {
+    const reader = new FileReader();
+
+    reader.addEventListener('load', () => {
+      const result = reader.result;
+      uploadFormImg.src = result;
+      uploadFormEffectPreviews.forEach((item) => item.style.backgroundImage = `url(${result})`);
+    });
+    reader.readAsDataURL(file);
+  }
+};
+
 const uploadFileChangeHandler = () => {
   openUploadForm();
+  uploadImage();
 };
 
 const onFormSendSuccess = () => {
